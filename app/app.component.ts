43260d4db9d16a6bb7ea23914108481cf6b2f08c
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-08-25 17:03:21 +0530

    (core) : Polymer included
    The polymer is now included to build the ui components till material2 is not available

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
 	moduleId : module.id,
 	selector : 'ng-app',
-	template : `<h1>Angular App<h1>`
+	template : `<h1>Angular App</h1>`
 })
 export class AppComponent { }
