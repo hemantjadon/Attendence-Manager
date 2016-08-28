d3de6a725644a65c7591bceb2c0f4daa5a6037d6
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-08-28 11:54:44 +0530

    (core) : migrating to polymer

--- app/dashboard/dashboard.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	moduleId : module.id,
-	templateUrl : "templates/dashboard.component.html",
-	styleUrls : ["styles/dashboard.component.css"]
-})
-export class DashboardComponent { }
--- app/home/home.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	moduleId : module.id,
-	selector : 'app-home',
-	template : `<h1>Home</h1>`
-})
-export class HomeComponent { }
